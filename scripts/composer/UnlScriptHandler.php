e409a6a08025a3e24003fc0056c834e77bc95127
Christopher Burge <1521132+macburgee1@users.noreply.github.com>
2019-03-26 09:16:09 -0500

    #10 Check if DevMode before deploying wdn to Drupal root

--- scripts/composer/UnlScriptHandler.php
+++ scripts/composer/UnlScriptHandler.php
@@ -15,37 +15,43 @@ class UnlScriptHandler {
    * Deploys the wdn directory to the correct path.
    */
   public static function deployWdn(Event $event) {
-    $io = $event->getIO();
 
-    $fs = new Filesystem();
-    $drupalFinder = new DrupalFinder();
-    $drupalFinder->locateRoot(getcwd());
-    $drupalRoot = $drupalFinder->getDrupalRoot();
-    $composerRoot = $drupalFinder->getComposerRoot();
+    // Check if dev dependencies are being installed.
+    // WDN is deployed in a different manner on production.
+    if ($event->isDevMode()) {
+      $io = $event->getIO();
 
-    // Symlink /vendor/unl/wdntemplates to web/wdn.
-    $fs->symlink('../vendor/unl/wdntemplates/wdn', 'web/wdn');
-    $io->write("WDN directory symlinked at " . $drupalRoot . "/wdn");
+      $fs = new Filesystem();
+      $drupalFinder = new DrupalFinder();
+      $drupalFinder->locateRoot(getcwd());
+      $drupalRoot = $drupalFinder->getDrupalRoot();
+      $composerRoot = $drupalFinder->getComposerRoot();
 
-    // Check if NPM is installed.
-    if (empty(exec("which npm"))) {
-      $io->write("NPM is not installed");
-      return;
+      // Symlink /vendor/unl/wdntemplates to web/wdn.
+      $fs->symlink('../vendor/unl/wdntemplates/wdn', 'web/wdn');
+      $io->write("WDN directory symlinked at " . $drupalRoot . "/wdn");
+
+      // Check if NPM is installed.
+      if (empty(exec("which npm"))) {
+        $io->write("NPM is not installed");
+        return;
+      }
+
+      // Install NPM project.
+      exec("cd $composerRoot/vendor/unl/wdntemplates; npm install");
+      $io->write("Node project installed at " . $composerRoot . "/vendor/unl/wdntemplates");
+
+      // Check if Grunt CLI is installed.
+      if (empty(exec("which grunt"))) {
+        $io->write("Grunt CLI is not installed. Run 'npm install -g grunt-cli'");
+        return;
+      }
+
+      // Run Grunt default task.
+      exec("cd $composerRoot/vendor/unl/wdntemplates; grunt");
+      $io->write("Grunt default task run at " . $composerRoot . "/vendor/unl/wdntemplates");
     }
 
-    // Install NPM project.
-    exec("cd $composerRoot/vendor/unl/wdntemplates; npm install");
-    $io->write("Node project installed at " . $composerRoot . "/vendor/unl/wdntemplates");
-
-    // Check if Grunt CLI is installed.
-    if (empty(exec("which grunt"))) {
-      $io->write("Grunt CLI is not installed. Run 'npm install -g grunt-cli'");
-      return;
-    }
-
-    // Run Grunt default task.
-    exec("cd $composerRoot/vendor/unl/wdntemplates; grunt");
-    $io->write("Grunt default task run at " . $composerRoot . "/vendor/unl/wdntemplates");
   }
 
 }
