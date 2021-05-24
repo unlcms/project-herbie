26d8af56c7107eea07cc97047a70c79f9e6f1dff
Christopher Burge <cburge2@unl.edu>
2021-05-24 11:34:55 -0500

    [#242] Added Queue UI 8.x-2.2; Add manual batch to SettingsForm

--- web/modules/custom/unl_news/src/Form/SettingsForm.php
+++ web/modules/custom/unl_news/src/Form/SettingsForm.php
@@ -5,12 +5,15 @@ namespace Drupal\unl_news\Form;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Queue\QueueFactory;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * The settings form for the Twig UI module.
+ * The settings form for the UNL News module.
  */
 class SettingsForm extends ConfigFormBase {
+
+  const QUEUE_NAME = 'nebraska_today_queue_processor';
 
   /**
    * The messenger service.
@@ -20,13 +23,23 @@ class SettingsForm extends ConfigFormBase {
   protected $messenger;
 
   /**
+   * The queue factory service.
+   *
+   * @var \Drupal\Core\Queue\QueueFactory
+   */
+  protected $queueFactory;
+
+  /**
    * Constructs a SettingsForm object.
    *
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    *   The messenger service.
+   * @param \Drupal\Core\Queue\QueueFactory $queue_factory
+   *   The queue factory service.
    */
-  public function __construct(MessengerInterface $messenger) {
+  public function __construct(MessengerInterface $messenger, QueueFactory $queue_factory) {
     $this->messenger = $messenger;
+    $this->queueFactory = $queue_factory;
   }
 
   /**
@@ -34,7 +47,8 @@ class SettingsForm extends ConfigFormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('messenger')
+      $container->get('messenger'),
+      $container->get('queue')
     );
   }
 
@@ -89,12 +103,28 @@ class SettingsForm extends ConfigFormBase {
       '#default_value' => $config->get('tag_ids'),
     ];
 
-    $form['markup'] = [
-      '#markup' => 'Test',
+    $form['queue'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Import Queue'),
     ];
 
-    // @todo provide ability to process on demand.
-    // @todo show items in queue.
+    /** @var \Drupal\Core\Queue\QueueInterface */
+    $queue = $this->queueFactory->get(self::QUEUE_NAME);
+
+    $form['queue']['queue_number_of_items'] = [
+      '#markup' => $this->t('<p><strong>Number of items in queue:</strong> @number.</p><p>Queue items are processed by cron. Depending on how many items are in queue, it may take several cron jobs cycles to process all of the items.</p>', ['@number' => $queue->numberOfItems()]),
+    ];
+
+    $form['queue']['manual_batch'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Process remaining items'),
+      '#name' => 'manual-batch',
+    ];
+
+    $form['queue']['manual_batch_description'] = [
+      '#markup' => $this->t('<p class="description">When manually processing items, do not close your browser window until completion.</p>'),
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -102,6 +132,9 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    if ($form_state->getTriggeringElement()['#name'] == 'manual-batch') {
+      return;
+    }
     parent::validateForm($form, $form_state);
   }
 
@@ -109,6 +142,10 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    if ($form_state->getTriggeringElement()['#name'] == 'manual-batch') {
+      $this->manualBatch();
+      return;
+    }
     $tag_ids = $form_state->getValue('tag_ids');
     $tag_ids = array_keys($tag_ids);
 
@@ -117,6 +154,25 @@ class SettingsForm extends ConfigFormBase {
     $config->save();
 
     parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * Initiates batch processing with Batch API.
+   *
+   * Replace with service when/if
+   * https://www.drupal.org/project/queue_ui/issues/3214399
+   * is committed.
+   */
+  public function manualBatch() {
+    $batch = [
+      'title' => $this->t('Processing queues'),
+      'operations' => [],
+      'finished' => ['\Drupal\queue_ui\QueueUIBatch', 'finish'],
+    ];
+
+    $batch['operations'][] = ['\Drupal\queue_ui\QueueUIBatch::step', [self::QUEUE_NAME]];
+
+    batch_set($batch);
   }
 
 }
