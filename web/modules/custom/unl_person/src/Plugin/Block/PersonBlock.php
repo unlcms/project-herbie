fccb5b678c5a05af00b6645607428d75518c72ee
Eric Rasmussen <ericrasmussen1@gmail.com>
2023-04-06 13:30:52 -0500

    Switch unl_person block to a select2 widget so that custom sort order can be preserved

--- web/modules/custom/unl_person/src/Plugin/Block/PersonBlock.php
+++ web/modules/custom/unl_person/src/Plugin/Block/PersonBlock.php
@@ -11,7 +11,7 @@ use Drupal\Core\Template\Attribute;
  *
  * @Block(
  *   id = "unl_person_list",
- *   admin_label = @Translation("Person"),
+ *   admin_label = @Translation("People"),
  *   category = @Translation("Curated Lists"),
  * )
  */
@@ -30,21 +30,52 @@ class PersonBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockForm($form, FormStateInterface $form_state) {
-    $person_nodes = \Drupal::entityTypeManager()->getStorage('node')
-      ->loadByProperties(['type' => 'person', 'status' => 1]);
-    $options = [];
-    foreach ($person_nodes as $node) {
-      $options[$node->id()] = $node->label();
-    }
+    $form = parent::blockForm($form, $form_state);
+
+    // The original 'select' type that doesn't allow custom sorting.
+    //    $person_nodes = \Drupal::entityTypeManager()->getStorage('node')
+    //      ->loadByProperties(['type' => 'person', 'status' => 1]);
+    //    $options = [];
+    //    foreach ($person_nodes as $node) {
+    //      $options[$node->id()] = $node->label();
+    //    }
+    //
+    //    $form['persons'] = [
+    //      '#type' => 'select',
+    //      '#title' => $this->t('Persons to display'),
+    //      '#required' => TRUE,
+    //      '#multiple' => TRUE,
+    //      '#default_value' => $this->configuration['persons'],
+    //      '#options' => $options,
+    //    ];
+
+    // Using entity_autocomplete. Not finished and doesn't allow easy sorting. Needs work on #default_value.
+    //     $form['persons'] = [
+    //       '#title' => $this->t('Persons to reference'),
+    //       '#type' => 'entity_autocomplete',
+    //       '#target_type' => 'node',
+    //       '#tags' => TRUE,
+    //       //'#default_value' => $this->configuration['persons'],
+    //       '#selection_handler' => 'default',
+    //       '#selection_settings' => [
+    //         'target_bundles' => ['person'],
+    //        ],
+    //     ];
 
     $form['persons'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Persons to display'),
+      '#type' => 'select2',
+      '#title' => $this->t('Person nodes to display'),
+      '#description' => $this->t('A person needs to exist as a <a href="/admin/content" target="_blank">Person node on the site</a> to be referenced here. People can be created through the <a href="/node/add" target="_blank">Add content</a> page.'),
+      '#default_value' => $this->configuration['persons'],
       '#required' => TRUE,
       '#multiple' => TRUE,
-      '#default_value' => $this->configuration['persons'],
-      '#options' => $options,
-    ];
+      '#autocomplete' => TRUE,
+      '#target_type' => 'node',
+      '#selection_handler' => 'default',
+      '#selection_settings' => [
+        'target_bundles' => ['person'],
+       ],
+     ];
 
     return $form;
   }
@@ -53,8 +84,10 @@ class PersonBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
-    $this->configuration['persons'] = $form_state
-      ->getValue('persons');
+    parent::blockSubmit($form, $form_state);
+    // Can't use $form_state->getValue('persons') as it gives the values sorted
+    // in a numerically ascending manner, not respecting user sorting.
+    $this->configuration['persons'] = $form_state->getCompleteFormState()->getUserInput()['settings']['persons'];
   }
 
   /**
