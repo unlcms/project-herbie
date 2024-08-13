79f0c83d5e4bab0420211d008579ebc55fee68d7
Saron Gebre <saron.solomon2000@gmail.com>
2024-08-13 16:54:22 -0500

    Commented scroll-animation javascript temporarily. Disabled the animation field for Simple Media block. Fixes #797.

--- web/themes/custom/unl_five_herbie/js/theme/photo_hero.js
+++ web/themes/custom/unl_five_herbie/js/theme/photo_hero.js
@@ -1,53 +1,55 @@
-window.addEventListener('inlineJSReady', function () {
-  if (define.amd === undefined) {
-    define.amd = define.origAmd;
-    delete define.origAmd;
-  }
-  window.WDNPluginsExecuting++;
+// Code block is commented until framework 6 update (removal of require.js)
 
-  WDN.initializePlugin(
-    'scroll-animations',
-    {},
-    photoHeroCallback,
-    'after'
-  );
+// window.addEventListener('inlineJSReady', function () {
+//   if (define.amd === undefined) {
+//     define.amd = define.origAmd;
+//     delete define.origAmd;
+//   }
+//   window.WDNPluginsExecuting++;
 
-  function photoHeroCallback() {
-    require(['plugins/gsap/gsap', 'plugins/gsap/ScrollTrigger'], (gsapModule, ScrollTriggerModule) => {
-      let animations = [];
-      let motionQuery = matchMedia('(prefers-reduced-motion)');
-      const gsap = gsapModule.gsap;
+//   WDN.initializePlugin(
+//     'scroll-animations',
+//     {},
+//     photoHeroCallback,
+//     'after'
+//   );
 
-      const handleReduceMotion = () => {
-        if (motionQuery.matches) {
-          animations.forEach((singleAnimation) => {
-            singleAnimation.progress(1).pause();
-          });
-        } else {
-          animations.forEach((singleAnimation) => {
-            singleAnimation.play();
-          });
-        }
-      };
+//   function photoHeroCallback() {
+//     require(['plugins/gsap/gsap', 'plugins/gsap/ScrollTrigger'], (gsapModule, ScrollTriggerModule) => {
+//       let animations = [];
+//       let motionQuery = matchMedia('(prefers-reduced-motion)');
+//       const gsap = gsapModule.gsap;
 
-      motionQuery.addEventListener('change', handleReduceMotion);
+//       const handleReduceMotion = () => {
+//         if (motionQuery.matches) {
+//           animations.forEach((singleAnimation) => {
+//             singleAnimation.progress(1).pause();
+//           });
+//         } else {
+//           animations.forEach((singleAnimation) => {
+//             singleAnimation.play();
+//           });
+//         }
+//       };
 
-      animations.push(
-        gsap.to('.unlcms-slow-zoom-in', {
-          scale: 1.13,
-          duration: 30,
-          paused: true
-        })
-      );
+//       motionQuery.addEventListener('change', handleReduceMotion);
 
-      handleReduceMotion();
+//       animations.push(
+//         gsap.to('.unlcms-slow-zoom-in', {
+//           scale: 1.13,
+//           duration: 30,
+//           paused: true
+//         })
+//       );
 
-      window.WDNPluginsExecuting--;
-      if (define.origAmd === undefined && window.WDNPluginsExecuting === 0) {
-        define.origAmd = define.amd;
-        delete define.amd;
-      }
-    });
-  }
+//       handleReduceMotion();
 
-}, false);
+//       window.WDNPluginsExecuting--;
+//       if (define.origAmd === undefined && window.WDNPluginsExecuting === 0) {
+//         define.origAmd = define.amd;
+//         delete define.amd;
+//       }
+//     });
+//   }
+
+// }, false);
